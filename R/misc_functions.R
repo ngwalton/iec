0ae106df9f4c3ce493fae93b692bbdc7cefe78b7
ngwalton <ngwalton@gmail.com>
2014-10-15 18:08:11 -0500

    Updated roxygen comments in misc_functions.R.

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -1,4 +1,4 @@
-# Miscellanious function to support working with iec functions.
+# Miscellaneous function to support working with iec functions.
 #
 # Authors: Nicholas G. Walton and Robert W. Howe
 # Created 14 Oct 2014
@@ -9,15 +9,19 @@
 
 #' Sensitivity of taxa to a gradient.
 #'
-#' \code{get_sens} returns the sensisitivity of each taxon in a community data
-#'  frame (\code{sp}) to a reference gradient (\code{gradient}). Taxa with more
-#'  detections below the midpoint (\code{mid}) will have a negative sensitivity
-#'  and vice versa.
+#' \code{get_sens} returns the sensitivity of each taxon in a community data
+#' frame (\code{sp}) to a reference gradient (\code{gradient}).
 #'
-#' @param sp A community data frame (sites as rows, taxa as columns).
-#' @param gradient A numeric vector of reference gradient scores (0-10),
-#'   one for each site.
+#' Sensitivity is defined here by first finding how many detections were made
+#' above and below the midpoint (\code{mid}) of the gradient, taking their
+#' difference, and finally dividing this by the number of detections. Taxa with
+#' more detections below (\code{mid}) than above will have a negative
+#' sensitivity and vice versa. Note that while function \code{\link{mk_brc}}
+#' requires that the reference gradient be scaled to 0-10, \code{get_sens} does
+#' not.
+#'
 #' @param mid A numeric scalar taken as the gradient's midpoint for sensitivity.
+#' @inheritParams mk_brc
 #' @return A data frame containing each taxon's sensitivity (\code{Sens}) to the
 #'   gradient, and the number of sites the they were detected at (\code{n}).
 #' @examples
@@ -26,12 +30,6 @@
 #' grad <- runif(10, 0, 10)
 #' get_sens(sp, grad)
 get_sens <- function(sp, gradient, mid = 5) {
-  # sp is a data frame containing species observations with
-  # rows as sites, and columns as taxa.
-  # gradient is a vector of reference gradient values
-  # at each site.
-  # Returns a data frame with taxa as rows, and columns Sens (sensitivity)
-  # N (numer of cites where the taxon was detected).
 
   sens <- function(sp_vec, grad) {
     pres <- sp_vec > 0
@@ -48,7 +46,7 @@ get_sens <- function(sp, gradient, mid = 5) {
 }
 
 
-## scale funciton ----
+## scale function ----
 
 #' Scale gradient to 0-10
 #'
@@ -56,16 +54,14 @@ get_sens <- function(sp, gradient, mid = 5) {
 #' Optionally, it can invert the scale.  This is essential for use with
 #' \link{get_brc}.
 #'
-#' @param gradient A numeric vector containing gradient values.
-#' @param invert Logical scalar indicatingin if the gradient should be inverted
+#' @param invert A logical scalar indicating if the gradient should be inverted
 #'   (defaults to \code{FALSE}).
+#' @inheritParams mk_brc
 #' @return A numeric vector with \code{gradient} scaled to 0-10.
 #' @examples
 #' grad <- runif(10)
 #' scale10(grad, TRUE)
 scale10 <- function(gradient, invert = FALSE) {
-  # Scales a numeric vector from 0 to 10.
-  # If 'invert = TRUE', the scale will be reversed.
   s <- 10 * ((gradient - min(gradient)) / (max(gradient) - min(gradient)))
   if (invert) s <- 10 - s
   s
